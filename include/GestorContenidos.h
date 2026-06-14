9ddd75542e10d3dfc134a6acf4f47d646a265542
LugpDev2022 <LugpDev2022@gmail.com>
2026-06-14 09:00:49 -0600

    feat: crear archivo de encabezado de la clase GestorContenidos

---
+++ include/GestorContenidos.h
@@ -0,0 +1,26 @@
+#ifndef GESTORCONTENIDOS_H
+#define GESTORCONTENIDOS_H
+
+#include "Contenido.h"
+
+#include <vector>
+
+using namespace std;
+
+class GestorContenidos
+{
+private:
+  vector<Contenido *> contenidos;
+
+public:
+  GestorContenidos();
+  void agregarContenido(Contenido *c);
+  void buscarPorId(string id);
+  void buscarPorTitulo(string t);
+  void eliminarContenido(string id);
+  void listarContenidos();
+  void listarContenidosPorGenero(Genero g);
+  void operator+(Contenido *c);
+};
+
+#endif // GESTORCONTENIDOS_H
