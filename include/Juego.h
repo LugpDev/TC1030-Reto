4ee3a6a8c71de41c24753e9bdd94937bbb19b1c7
LugpDev2022 <LugpDev2022@gmail.com>
2026-06-14 08:06:33 -0600

    feat(Juego): crear archivo de encabezado para la clase Juego

---
+++ include/Juego.h
@@ -0,0 +1,26 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+
+#include "./Contenido.h"
+
+enum class Dificultad
+{
+  FACIL,
+  MEDIO,
+  DIFICIL
+};
+
+class Juego : public Contenido
+{
+private:
+  bool offline;
+  bool multijugador;
+  Dificultad dificultad;
+
+public:
+  Juego(string _id, string _titulo, string _genero, bool _offline, bool _multijugador, Dificultad _dificultad);
+  void mostrarInfo();
+  void reproducir();
+};
+
+#endif // JUEGO_H
