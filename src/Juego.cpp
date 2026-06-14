e9f9059071525fab4a2c40af9c10a8dfdada0988
LugpDev2022 <LugpDev2022@gmail.com>
2026-06-14 08:12:57 -0600

    feat: implementar clase Juego

---
+++ src/Juego.cpp
@@ -0,0 +1,40 @@
+#include "../include/Juego.h"
+
+#include <iostream>
+
+using namespace std;
+
+Juego::Juego(string _id, string _titulo, string _genero, bool _offline, bool _multijugador, Dificultad _dificultad) : Contenido(_id, _titulo, _genero)
+{
+  offline = _offline;
+  multijugador = _multijugador;
+  dificultad = _dificultad;
+}
+
+void Juego::mostrarInfo()
+{
+  cout << "ID: " << id << endl;
+  cout << "Titulo: " << titulo << endl;
+  cout << "Genero: " << genero << endl;
+  cout << "Offline: " << (offline ? "Si" : "No") << endl;
+  cout << "Multijugador: " << (multijugador ? "Si" : "No") << endl;
+  cout << "Dificultad: ";
+  switch (dificultad)
+  {
+  case Dificultad::FACIL:
+    cout << "Facil";
+    break;
+  case Dificultad::MEDIO:
+    cout << "Medio";
+    break;
+  case Dificultad::DIFICIL:
+    cout << "Dificil";
+    break;
+  }
+  cout << endl;
+}
+
+void Juego::reproducir()
+{
+  cout << "Reproduciendo juego: " << titulo << endl;
+}
